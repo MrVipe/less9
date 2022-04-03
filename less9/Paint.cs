ab2800c79b20df442e0bace3f864f47e03799904
user <user@HOME-PC>
2022-04-04 00:34:00 +0400

    Добавьте файлы проекта.

---
+++ less9/Paint.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    internal class Paint
+    {
+
+        private static string? path;
+        private static int level = 0;
+        private static int maxLevel = 0;
+
+        public Paint(string path, int islevel)
+        {
+            Path = path;
+            Islevel = islevel;
+        }
+
+        public string? Path
+        {
+            get => path;
+            set => path = value;
+        }
+
+        public int Islevel
+        {
+            get => level;
+            set => level = value;
+        }
+
+        public int MaxLevel
+        {
+            get => maxLevel;
+            set => maxLevel = value;
+        }
+
+        public void RePaintAll(string path, int index) //перерисовываем все
+        {
+            Console.Clear();
+            RePaintTop(path, index);
+            RePaintStructure(path, index);
+        }
+
+        private static void RePaintTop(string path, int index) //перерисовываем верх
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+            Console.WriteLine(path);
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+        }
+
+        static void RePaintStructure(string oldPath, int index) //перерисовываем каталоги
+        {
+            if (Directory.Exists(oldPath))
+            {
+                ConsColr("..", index == -1);
+                string[] getDir = Directory.GetDirectories(oldPath); //получаем директории
+                maxLevel = getDir.Length - 1;
+                if (getDir.Length != 0)
+                {
+                    int curIndex = 0;
+                    foreach (string item in getDir) //проходы по директориям
+                    {
+                        for (int j = 0; j < item.Length; j++)
+                        {
+                            string pathSmall = "";
+                            for (int k = item.Length - 1; k > 0; k--)
+                            {
+                                if (item[k] != Convert.ToChar(@"\"))
+                                {
+                                    pathSmall = item[k] + pathSmall;
+                                }
+                                else break;
+
+                            }
+                            ConsColr(pathSmall, (curIndex == index));
+                            break;
+                        }
+                        curIndex++;
+                    }
+                }
+            }
+
+        }
+
+        public string GetNewCatalog(string oldPath,int index)//переходим в новый каталог
+        {
+            if (index != -1)
+            {
+                string[] getDir = Directory.GetDirectories(oldPath); //получаем директории
+                return getDir[index];
+            }
+            else
+            {
+                return getDir[index];
+            }
+        }
+
+        static void ConsColr(string curPath, bool isMyRow) //отрисовка выбранного каталога
+        {
+
+            if (isMyRow)
+            {
+                Console.WriteLine(curPath + @"         <0-----<");
+            }
+            else
+            {
+                Console.WriteLine(curPath);
+            }
+
+        }
+
+    }
+}
---
+++ less9/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyApp
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Program Files (x86)\";
+            int indexRow = 0;
+            Manager.Paint myManager = new Manager.Paint(path, indexRow);
+
+       
+
+            if (Directory.Exists(path))
+            {
+                while (true)
+                {
+                    myManager.RePaintAll(path, indexRow);
+                    ConsoleKeyInfo myCommand = Console.ReadKey();
+                    switch (myCommand.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            {
+                                if (indexRow >= 0) { indexRow--; }
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            {
+                                if (indexRow < myManager.MaxLevel) { indexRow++; }
+                            }
+                            break;
+                        case ConsoleKey.Enter:
+                            {
+                                path = myManager.GetNewCatalog(path, indexRow);
+                                indexRow = -1;
+                            }
+                            break;
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+    
+
+        // string[] getFile = Directory.GetFiles(oldPath).Select(Path.GetFileName).ToArray();
+ 
+        static string getParentPath(string pathChild, int thislvl)
+        {
+            int lvl = 0;
+            string path = "";
+            for (int i = 0; i < pathChild.Length; i++)
+            {
+                path += pathChild[i];
+                if ((Directory.Exists(path)) & (thislvl == lvl))
+                {
+                    return path + @"\";
+                }
+            }
+            return "";
+        }
+        static string StrProbel(int lvl) //красота
+        {
+            string probel = "";
+            for (int i = 0; i < lvl; i++)
+            {
+                probel += " ";
+            }
+
+            return probel;
+        }
+
+
+        //static void GetNewPath(int lvl, string oldPath)
+        //{
+        //    File.AppendAllText("recurDir.txt", StrProbel(lvl) + oldPath + Environment.NewLine);// Записываем директорию
+        //    string[] getDir = Directory.GetDirectories(oldPath); //получаем директорию
+        //    string[] getFile = Directory.GetFiles(oldPath).Select(Path.GetFileName).ToArray(); //получаем файлы в тек. директории
+        //    for (int i = 0; i < getFile.Length; i++)
+        //    {
+        //        getFile[i] = StrProbel(lvl + 5) + getFile[i];
+        //    }
+        //    File.AppendAllLines("recurDir.txt", getFile); //записываем файлы
+
+        //    if (getDir.Length != 0)
+        //    {
+        //        foreach (string item in getDir) //проходы по директориям
+        //        {
+        //            GetNewPath(lvl + 3, item);
+        //        }
+        //    }
+        //}
+
+    }
+}
