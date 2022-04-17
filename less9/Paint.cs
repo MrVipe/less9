c85fa891c7b3be291f65e8e6913a77abcdd3331d
gurgur1754@gmail.com <gurgur1754@gmail.com>
2022-04-18 00:31:14 +0400

    12

--- less9/Paint.cs
+++ less9/Paint.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Manager
 {
@@ -12,19 +13,30 @@ namespace Manager
         private static string? path;
         private static int level = 0;
         private static int maxLevel = 0;
+        private static int catalogPerStage = 15;
+        private static int itemInDirector = 0;
 
         public Paint(string path, int islevel)
         {
-            Path = path;
+            MyPath = path;
             Islevel = islevel;
         }
+        public int ItemInDirector
+        {
+            get => itemInDirector;
+            set => itemInDirector = value;
+        }
 
-        public string? Path
+        public string? MyPath
         {
             get => path;
             set => path = value;
         }
 
+        public int CatalogPerStage
+        {
+            get => catalogPerStage;
+        }
         public int Islevel
         {
             get => level;
@@ -37,11 +49,11 @@ namespace Manager
             set => maxLevel = value;
         }
 
-        public void RePaintAll(string path, int index) //перерисовываем все
+        public void RePaintAll(string path, int index, int stage) //перерисовываем все
         {
             Console.Clear();
             RePaintTop(path, index);
-            RePaintStructure(path, index);
+            RePaintStructure(path, index, stage);
         }
 
         private static void RePaintTop(string path, int index) //перерисовываем верх
@@ -59,36 +71,41 @@ namespace Manager
             Console.WriteLine("");
         }
 
-        static void RePaintStructure(string oldPath, int index) //перерисовываем каталоги
+        static void RePaintStructure(string oldPath, int index, int stage) //перерисовываем каталоги
         {
-            if (Directory.Exists(oldPath))
+            if (Directory.Exists(oldPath) )
             {
-                ConsColr("..", index == -1);
+                if (stage == 0) { ConsColr("..", index == -1); }
                 string[] getDir = Directory.GetDirectories(oldPath); //получаем директории
-                maxLevel = getDir.Length - 1;
-                if (getDir.Length != 0)
+                string[] getFile = Directory.GetFiles(oldPath); //получаем файлы
+                string[] fullItem = getDir.Concat(getFile).ToArray();
+                Manager.Paint.itemInDirector = fullItem.Length;
+                maxLevel = fullItem.Length - 1;
+                if (itemInDirector != 0)
                 {
-                    int curIndex = 0;
-                    foreach (string item in getDir) //проходы по директориям
+                    for (int i = 0 + stage * catalogPerStage; i < itemInDirector && i < (stage + 1) * catalogPerStage; i++)
                     {
-                        for (int j = 0; j < item.Length; j++)
-                        {
-                            string pathSmall = "";
-                            for (int k = item.Length - 1; k > 0; k--)
-                            {
-                                if (item[k] != Convert.ToChar(@"\"))
-                                {
-                                    pathSmall = item[k] + pathSmall;
-                                }
-                                else break;
-
-                            }
-                            ConsColr(pathSmall, (curIndex == index));
-                            break;
-                        }
-                        curIndex++;
+                        ConsColr(Path.GetFileName(fullItem[i]), (i == index));
                     }
+                    ConsStagePaint(itemInDirector, stage);
                 }
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+        }
+
+        private static void ConsStagePaint(int length, int stage)
+        {
+            int cur = ((stage + 1) * catalogPerStage);
+            cur = (cur > length) ? length : cur;
+            if (length > catalogPerStage)
+            {
+                Console.WriteLine("//--------------------------//");
+                Console.WriteLine($"Показано запись {cur} из {length}");
+                Console.WriteLine("//--------------------------//");
             }
 
         }
@@ -102,8 +119,17 @@ namespace Manager
             }
             else
             {
-                return getDir[index];
+                return GetUpPath(oldPath); //получаем директории
             }
+        }
+
+        public string GetUpPath(string oldpath) //На папку назад
+        {
+            string? newPath;
+            newPath = Path.GetDirectoryName(oldpath);
+
+            return (newPath != null) ? newPath : oldpath;
+
         }
 
         static void ConsColr(string curPath, bool isMyRow) //отрисовка выбранного каталога
@@ -111,7 +137,7 @@ namespace Manager
 
             if (isMyRow)
             {
-                Console.WriteLine(curPath + @"         <0-----<");
+                Console.WriteLine(curPath + @" < 0-----<");
             }
             else
             {
--- less9/Program.cs
+++ less9/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace MyApp
 {
@@ -13,47 +16,244 @@ namespace MyApp
         static void Main(string[] args)
         {
 
-            string path = @"C:\Program Files (x86)\";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            string? path = @"";
+            try
+            {
+                string loadJson = File.ReadAllText("data.json");
+                path = JsonConvert.DeserializeObject<String>(loadJson);
+            }
+            catch (Exception)
+            {
+                path = @"C:\";
+            }
             int indexRow = 0;
             Manager.Paint myManager = new Manager.Paint(path, indexRow);
+            int currentStage = 0;
 
-       
+
 
             if (Directory.Exists(path))
             {
                 while (true)
                 {
-                    myManager.RePaintAll(path, indexRow);
+                    System.Threading.Thread.Sleep(100);
+                    myManager.RePaintAll(path, indexRow, currentStage);
                     ConsoleKeyInfo myCommand = Console.ReadKey();
-                    switch (myCommand.Key)
+                    if ((myCommand.Key == ConsoleKey.UpArrow) || (myCommand.Key == ConsoleKey.DownArrow) ||
+                            (myCommand.Key == ConsoleKey.LeftArrow) || (myCommand.Key == ConsoleKey.RightArrow) ||
+                            (myCommand.Key == ConsoleKey.Enter))
                     {
-                        case ConsoleKey.UpArrow:
-                            {
-                                if (indexRow >= 0) { indexRow--; }
-                            }
-                            break;
-                        case ConsoleKey.DownArrow:
-                            {
-                                if (indexRow < myManager.MaxLevel) { indexRow++; }
-                            }
-                            break;
-                        case ConsoleKey.Enter:
-                            {
-                                path = myManager.GetNewCatalog(path, indexRow);
-                                indexRow = -1;
-                            }
-                            break;
+                        switch (myCommand.Key)
+                        {
+                            case ConsoleKey.UpArrow:
+                                {
+                                    if (indexRow == 0 || indexRow > (currentStage) * myManager.CatalogPerStage) { indexRow--; }
+                                }
+                                break;
+                            case ConsoleKey.DownArrow:
+                                {
+                                    if ((indexRow < (currentStage + 1) * myManager.CatalogPerStage - 1 || (indexRow == -1)) && indexRow < myManager.MaxLevel) { indexRow++; }
+                                }
+                                break;
+                            case ConsoleKey.RightArrow:
+                                {
+                                    if ((currentStage + 1) * myManager.CatalogPerStage < myManager.ItemInDirector)
+                                    {
+                                        currentStage++;
+                                        indexRow = currentStage * myManager.CatalogPerStage;
+                                    }
+                                }
+                                break;
+                            case ConsoleKey.LeftArrow:
+                                {
+                                    if (currentStage > 0)
+                                    {
+                                        currentStage--;
+                                        indexRow = currentStage * myManager.CatalogPerStage;
+                                    }
+                                }
+                                break;
+                            case ConsoleKey.Enter:
+                                {
+                                    bool isErrorAccess = false;
+                                    try
+                                    {
+                                        string[] getDir = Directory.GetDirectories(myManager.GetNewCatalog(path, indexRow));
+                                        isErrorAccess = false;
+                                    }
+                                    catch (Exception err)
+                                    {
+                                        if (err.HResult == -2146233080)
+                                        {
+                                            isErrorAccess = true;
+                                        }
+                                        else
+                                        { 
+                                            Console.WriteLine("Отказано в доуступе: " + err.Message);
+                                            isErrorAccess = true;
+                                            Console.ReadKey();
+                                        }
+                                    }
+
+                                    if (isErrorAccess == false)
+                                    {
+                                        currentStage = 0;
+                                        path = myManager.GetNewCatalog(path, indexRow);
+                                        string json = JsonConvert.SerializeObject(path); //заносим через Json в файл
+                                        File.WriteAllText("data.json", json);
+                                        indexRow = -1;
+                                    }
+
+                                }
+                                break;
+                        }
                     }
+                    else
+                    {
+                        Console.WriteLine("");
+                        string? myCommand2 = Console.ReadLine();
+                        if (myCommand2[0] != '-')
+                        {
+                            Console.WriteLine("Неизвестная команда. Для отображение доступных команд наберите -hp");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            string[]? regOut = RegularGo(myCommand2);
+
+                            switch (regOut[0])
+                            {
+                                case "hp":
+                                    {
+                                        Console.WriteLine("Перейти в другой каталог -cd <путь>");
+                                        Console.WriteLine("Создать директорию в текущем каталоге -md <название>");
+                                        Console.WriteLine("Удалить директорию -kd <путь>");
+                                        Console.WriteLine("Удалить файл в текущем каталоге -kf <название>");
+                                        Console.WriteLine("Копировать директорию -cd <откуда> <куда>");
+                                        Console.WriteLine("Копировать файл -cf <откуда> <куда>");
+                                        Console.ReadKey();
+                                    }
+                                    break;
+                                case "cd":
+                                    {
+
+                                        if (Directory.Exists(regOut[1]) == false)
+                                        {
+                                            Console.WriteLine($"Такого пути не существует: {regOut[1]}");
+                                            Console.ReadKey();
+                                        }
+                                        else
+                                        {
+                                            path = regOut[1];
+                                            indexRow = 0;
+                                            currentStage = 0;
+                                        }
+                                    }
+                                    break;
+                                case "md":
+                                    {
+
+                                        if (Directory.Exists(path + regOut[1] + @"\") == true)
+                                        {
+                                            Console.WriteLine($"Такой каталог уже существует: {path + regOut[1]}");
+                                            Console.ReadKey();
+                                        }
+                                        else
+                                        {
+                                            try
+                                            {
+                                                Directory.CreateDirectory(path + regOut[1] + @"\");
+                                                path = path + regOut[1];
+                                                indexRow = -1;
+                                                currentStage = 0;
+                                            }
+                                            catch (Exception err)
+                                            {
+                                                Console.WriteLine("Упс. Произошла ошибка: " + err.Message);
+                                            }
+
+                                        }
+                                    }
+                                    break;
+                                case "kd":
+                                    {
+
+                                        if (Directory.Exists(regOut[1]) == false)
+                                        {
+                                            Console.WriteLine($"Такого каталога не существует: {path + regOut[1]}");
+                                            Console.ReadKey();
+                                        }
+                                        else
+                                        {
+                                            try
+                                            {
+                                                Directory.Delete(regOut[1], true);
+                                                Console.WriteLine($"Каталог удален: {regOut[1]}");
+                                                Console.ReadKey();
+                                            }
+                                            catch (Exception err)
+                                            {
+                                                Console.WriteLine("Упс. Произошла ошибка: " + err.Message);
+                                            }
+
+                                        }
+                                    }
+                                    break;
+                                case "kf":
+                                    {
+                                        if (File.Exists(path + regOut[1]) == false)
+                                        {
+                                            Console.WriteLine($"Такого файла не существует: {path + regOut[1]}");
+                                            Console.ReadKey();
+                                        }
+                                        else
+                                        {
+                                            try
+                                            {
+                                                Directory.Delete(path + regOut[1], true);
+                                                Console.WriteLine($"Файл удален: {path + regOut[1]}");
+                                                Console.ReadKey();
+                                            }
+                                            catch (Exception err)
+                                            {
+                                                Console.WriteLine("Упс. Произошла ошибка: " + err.Message);
+                                            }
+
+                                        }
+                                    }
+                                    break;
+                                default:
+                                    {
+                                        Console.WriteLine("Неизвестная команда. Для отображение доступных команд наберите -hp");
+                                        Console.ReadKey();
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+
                 }
             }
             Console.ReadKey();
         }
 
 
-    
+        static string[]? RegularGo(string text)
+        {
+            string[]? outadata = new string[3];
+            Match match = Regex.Match(text, "\\-([a-z]{2})\\s?([a-z.\\:]*)?\\s?([a-z\\.:]*)?", RegexOptions.IgnoreCase);
+
+            outadata[0] = match.Groups[1].Value;
+            outadata[1] = match.Groups[2].Value;
+            outadata[2] = match.Groups[3].Value;
+
+            return outadata;
+        }
 
         // string[] getFile = Directory.GetFiles(oldPath).Select(Path.GetFileName).ToArray();
- 
+
         static string getParentPath(string pathChild, int thislvl)
         {
             int lvl = 0;
